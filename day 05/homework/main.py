a9a9720490d0c22e127926290223ab6a8562cd39
NikaM77111 <nikamachavariani771@gmail.com>
2025-03-12 20:53:38 +0100

    commit

---
+++ day 05/homework/main.py
@@ -0,0 +1,20 @@
+<h1>hello world</h1>
+<h2>hello world</h2>
+<h3>hello world</h3>
+<h4>hello world</h4>
+<h5>hello world</h5>
+<h6>hello world</h6>
+  
+
+<p>goa is good</p>
+<p>this is a paragraph</p>
+
+
+</b>button>
+</b>this is a button tag>
+
+
+front end aris visualuri mxare sadac momxmarebeli xedavs saits
+
+
+back end aris daxuruli mxare gavs theatris kulisebs momxmarebeli ver xedavs namushevars
---
+++ day 06/homework/main.py
@@ -0,0 +1,12 @@
+name="barbare"
+print(name)
+age=11
+print(age)
+hello world=hello
+print(hello world)
+name="nika"
+print(name)
+age=17
+print(age)
+year=2025
+print(year)
